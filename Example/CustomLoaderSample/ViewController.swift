3e89767d2dbf9aca58204a90f91469a686ade606
MUISEROUX Fritzgerald <f.muiseroux@gmail.com>
2017-01-30 08:34:41 +0100

    Add support for determinate progressRing

--- Example/CustomLoaderSample/ViewController.swift
+++ Example/CustomLoaderSample/ViewController.swift
@@ -11,6 +11,10 @@ import CustomLoader
 
 class ViewController: UIViewController {
     
+    var timer: Timer!
+    var value: CGFloat = -2
+    var loader: LoadingView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -22,15 +26,33 @@ class ViewController: UIViewController {
     
     @IBAction func showLoader(sender: Any) {
         
-        let loader = LoadingView.standardProgressBox.show(inView: view) { _ in
-            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+        value = -2
+        loader = LoadingView.standardProgressBox.show(inView: view) { _ in
+            DispatchQueue.main.asyncAfter(deadline: .now() + 10) {
                 self.view.removeLoadingViews(animated: true)
             }
         }
         
+        timer = Timer(timeInterval: 1, target: self, selector: #selector(timerFire), userInfo: nil, repeats: true)
+        RunLoop.main.add(timer, forMode: .defaultRunLoopMode)
+        
         if let box = loader.progressBox {
             box.label.text = "Loading ..."
             box.subLabel.text = "1 of 4"
+            if let ring = box.loaderView as? ProgressRingView {
+                ring.minimumValue = -2
+                ring.maximumValue = 2
+                ring.value = self.value
+            }
+        }
+    }
+    
+    func timerFire() {
+        if let box = loader.progressBox {
+            if let ring = box.loaderView as? ProgressRingView {
+                self.value += 0.4
+                ring.value = self.value
+            }
         }
     }
 }
